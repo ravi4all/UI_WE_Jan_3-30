0778a0668ff96ec5d704e722a5068f25bd3f6a9b
ravi4all <ravikanttyagi.2295@gmail.com>
2018-04-29 15:52:09 +0530

    Add files via upload

--- Javascript/MusicApp/script.js
+++ Javascript/MusicApp/script.js
@@ -29,10 +29,19 @@ var songList = [
 ]
 
 var audio;
-
+var play_btn;
+var slider;
 function initEvents(){
     var ul  =document.getElementById("songsList");
     audio = document.getElementById("audio");
+    play_btn = document.getElementById("play-btn");
+    play_btn.addEventListener("click", startSong);
+    pause_btn = document.getElementById("pause-btn");
+    pause_btn.addEventListener("click", pauseSong);
+
+    slider = document.getElementById("slider");
+    slider.addEventListener("change", seekSong);
+
 
     songList.forEach(function(obj){
         var li = document.createElement('li');
@@ -50,4 +59,25 @@ function playSong(){
     console.log(songUrl);
     audio.src = songUrl;
     audio.play();
+    song_timer = setInterval(function(){
+        audio_currentTime = audio.currentTime;
+        // console.log(audio_currentTime);
+        slider.value = audio_currentTime;
+    },1000);
+    // console.log(audio.duration);
+    setTimeout(function(){
+        slider.max = audio.duration;
+    },1000);
+}
+
+function startSong(){
+    audio.play();
+}
+
+function pauseSong(){
+    audio.pause();
+}
+
+function seekSong(){
+    audio.currentTime = slider.value;
 }
