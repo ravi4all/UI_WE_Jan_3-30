d319a88861da50f5be7b7feff7b2c71476aeea7d
ravi4all <ravikanttyagi.2295@gmail.com>
2018-05-19 15:29:38 +0530

    Add files via upload

--- Javascript/CRUD/controller/controller.js
+++ Javascript/CRUD/controller/controller.js
@@ -10,7 +10,25 @@ function initEvents(){
     p_desc = document.querySelector("#p_desc");
 
     document.querySelector("#add").addEventListener("click", addItem);
+    document.querySelector("#delete").addEventListener("click",deleteItem);
+    document.querySelector("#save").addEventListener("click", saveData);
+    document.querySelector("#load").addEventListener("click",loadData);
+}
 
+function saveData(){
+    if(window.localStorage){
+        var json = JSON.stringify(obj.itemList);
+        localStorage.setItem('data',json);
+    }
+    else {
+        alert("Localstorage not supported");
+    }
+}
+
+function loadData(){
+    var data = localStorage.getItem('data');
+    obj.itemList = JSON.parse(data);
+    printItems();
 }
 
 function addItem(){
@@ -28,10 +46,43 @@ function addItem(){
     li.appendChild(cart_btn);
     ul.appendChild(li);
     li.addEventListener("click", markItem);
+    clear_boxes();
+}
+
+function clear_boxes(){
+    p_name.value = "";
+    p_desc.value = "";
+    p_price.value = "";
 }
 
 function markItem(){
     event.srcElement.classList.toggle("marked");
     var current_id = event.srcElement.innerHTML.split(" ")[0];
     obj.toggleItem(current_id);
+}
+
+function deleteItem(){
+    obj.deleteItem();
+    printItems();
+}
+
+function printItems(){
+    var ul = document.querySelector("#itemList");
+    ul.innerHTML = "";
+
+    obj.itemList.forEach(function(x){
+        var li = document.createElement("li");
+        var cart_btn = document.createElement("button");
+        cart_btn.innerHTML = "Add to cart";
+    
+        li.setAttribute('class','list-group-item');
+        cart_btn.className = 'btn btn-success';
+    
+        obj.addItem(p_name.value,p_price.value,p_desc.value);
+    
+        li.innerHTML = x.id + " " + x.name +" " +x.price + " "+x.desc;
+        li.appendChild(cart_btn);
+        ul.appendChild(li);
+        li.addEventListener("click", markItem);
+    });
 }
