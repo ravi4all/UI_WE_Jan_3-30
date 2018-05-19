b7e680c7a50f9266b3b8a8cd06a64e911cb9a0b6
ravi4all <ravikanttyagi.2295@gmail.com>
2018-05-19 15:29:48 +0530

    Add files via upload

---
+++ Jquery/DOM_Traversing/dom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $("button").click(function(){
+        // $(this).siblings("img").hide(1000);
+        // $(this).parent().hide(1000);
+        // $(this).parents().hide(1000);
+        // $(this).parentsUntil('#parent_1').hide(1000);
+        // $(this).parents("#parent_1").siblings("#parent_2").find("img").hide(1000);
+        $(this).parents("#parent_1").siblings("#parent_2").find(".child:last-child img").hide(1000);
+    })
+
+})
