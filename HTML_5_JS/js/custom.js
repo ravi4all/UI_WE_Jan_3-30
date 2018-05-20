031ed1d95a7a992442551e0981b39435cd84a53c
ravi4all <ravikanttyagi.2295@gmail.com>
2018-05-20 15:25:48 +0530

    Add files via upload

---
+++ HTML_5_JS/js/custom.js
@@ -0,0 +1,8 @@
+function techknow(){
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  // Great success! All the File APIs are supported.
+  alert('Great success! All the File APIs are supported.');
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}
+}
---
+++ HTML_5_JS/js/workers.js
@@ -0,0 +1,5 @@
+for (var i = 0; i <= 10000000000; i += 1){
+   var j = i;
+}
+//alert("loop ends");
+postMessage(j);
